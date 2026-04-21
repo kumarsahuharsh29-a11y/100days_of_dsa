ed99c5d6262f4d35b1bb59d047f8ef841183adb2
kumarsahuharsh29-a11y <kumarsahuharsh29@gmail.com>
2026-04-21 23:31:19 +0530

    Add files via upload

---
+++ d13.c
@@ -0,0 +1,45 @@
+#include <stdlib.h>
+
+int* spiralOrder(int** matrix, int matrixSize, int* matrixColSize, int* returnSize) {
+    if (matrixSize == 0) {
+        *returnSize = 0;
+        return NULL;
+    }
+
+    int rows = matrixSize;
+    int cols = matrixColSize[0];
+    int total = rows * cols;
+    int* result = (int*)malloc(sizeof(int) * total);
+    *returnSize = 0;
+
+    int top = 0, bottom = rows - 1;
+    int left = 0, right = cols - 1;
+
+    while (top <= bottom && left <= right) {
+        for (int i = left; i <= right; i++) {
+            result[(*returnSize)++] = matrix[top][i];
+        }
+        top++;
+
+        for (int i = top; i <= bottom; i++) {
+            result[(*returnSize)++] = matrix[i][right];
+        }
+        right--;
+
+        if (top <= bottom) {
+            for (int i = right; i >= left; i--) {
+                result[(*returnSize)++] = matrix[bottom][i];
+            }
+            bottom--;
+        }
+
+        if (left <= right) {
+            for (int i = bottom; i >= top; i--) {
+                result[(*returnSize)++] = matrix[i][left];
+            }
+            left++;
+        }
+    }
+
+    return result;
+}
